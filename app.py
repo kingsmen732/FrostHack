6f229f594d14b221f618b2cfba002d9a151b48f5
kingsmen732 <mitumithilesh2@gmail.com>
2024-01-20 17:48:14 +0530

    mistakes removed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import langchain-community
+from langchain.callbacks import get_openai_callback
 import os
  
 # Sidebar contents
